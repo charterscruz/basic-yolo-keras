d3968c07d96798ec415eaac426a1a07ac4254dc4
charterscruz <g.charters.cruz@gmail.com>
2017-09-22 06:18:07 +0100

    Got the code in the notebook into a python script

---
+++ basic_yolo_keras.py
@@ -0,0 +1,100 @@
+from keras.models import Sequential
+from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense
+from keras.layers.advanced_activations import LeakyReLU
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from keras.optimizers import SGD
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import utils
+
+wt_path = 'tiny-yolo-voc.weights'
+ann_dir = '/home/husky/data/pascal/VOCdevkit/VOC2012/Annotations/'
+img_dir = '/home/husky/data/pascal/VOCdevkit/VOC2012/JPEGImages/'
+
+execfile('utils.py')
+
+LABELS = ['aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus', 'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike', 'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tvmonitor']
+COLORS = [(43,206,72),(255,204,153),(128,128,128),(148,255,181),(143,124,0),(157,204,0),(194,0,136),(0,51,128),(255,164,5),(255,168,187),(66,102,0),(255,0,16),(94,241,242),(0,153,143),(224,255,102),(116,10,255),(153,0,0),(255,255,128),(255,255,0),(255,80,5)]
+
+# 4
+NORM_H, NORM_W = 416, 416
+GRID_H, GRID_W = 13 , 13
+BATCH_SIZE = 8
+BOX = 5
+CLASS = 20
+THRESHOLD = 0.2
+ANCHORS = '1.08,1.19,  3.42,4.41,  6.63,11.38,  9.42,5.11,  16.62,10.52'
+ANCHORS = [float(ANCHORS.strip()) for ANCHORS in ANCHORS.split(',')]
+SCALE_NOOB, SCALE_CONF, SCALE_COOR, SCALE_PROB = 0.5, 5.0, 5.0, 1.0
+weight_reader = WeightReader(wt_path)
+
+model = Sequential()
+
+# Layer 1
+model.add(Conv2D(16, (3,3), strides=(1,1), padding='same', use_bias=False, input_shape=(416,416,3)))
+model.add(BatchNormalization())
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Layer 2 - 5
+for i in range(0,4):
+    model.add(Conv2D(32*(2**i), (3,3), strides=(1,1), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.1))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# Layer 6
+model.add(Conv2D(512, (3,3), strides=(1,1), padding='same', use_bias=False))
+model.add(BatchNormalization())
+model.add(LeakyReLU(alpha=0.1))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1,1), padding='same'))
+
+# Layer 7 - 8
+for _ in range(0,2):
+    model.add(Conv2D(1024, (3,3), strides=(1,1), padding='same', use_bias=False))
+    model.add(BatchNormalization())
+    model.add(LeakyReLU(alpha=0.1))
+
+# Layer 9
+model.add(Conv2D(BOX * (4 + 1 + CLASS), (1, 1), strides=(1, 1), kernel_initializer='he_normal'))
+model.add(Activation('linear'))
+model.add(Reshape((GRID_H, GRID_W, BOX, 4 + 1 + CLASS)))
+
+
+# 5
+model.summary()
+
+# 14
+
+weight_reader.reset()
+
+for i in range(len(model.layers)):
+    if 'conv' in model.layers[i].name:
+        if 'batch' in model.layers[i + 1].name:
+            norm_layer = model.layers[i + 1]
+            size = np.prod(norm_layer.get_weights()[0].shape)
+
+            beta = weight_reader.read_bytes(size)
+            gamma = weight_reader.read_bytes(size)
+            mean = weight_reader.read_bytes(size)
+            var = weight_reader.read_bytes(size)
+
+            weights = norm_layer.set_weights([gamma, beta, mean, var])
+
+        conv_layer = model.layers[i]
+        if len(conv_layer.get_weights()) > 1:
+            bias = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+            kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+            kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+            kernel = kernel.transpose([2, 3, 1, 0])
+            conv_layer.set_weights([kernel, bias])
+        else:
+            kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+            kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+            kernel = kernel.transpose([2, 3, 1, 0])
+            conv_layer.set_weights([kernel])
+
+
+# 16
+all_img = parse_annotation(ann_dir)
